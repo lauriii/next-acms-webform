261ef481ce2b4317dcad2890fe9f5e63d66a0528
HARUMI JANG <harumi.jang@acquia.com>
2022-11-04 17:10:44 -0400

    Added next i18next and pass down translation object

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const {i18n} = require('./next-i18next.config');
+
 module.exports = {
   images: {
     domains: [process.env.NEXT_IMAGE_DOMAIN ?? ''],
   },
   experimental: {
     externalDir: true,
+  },
+  i18n: {
+    locales: ['en', 'de'],
+    defaultLocale: 'de',
   },
 };
--- pages/webform/[webform_id].tsx
+++ pages/webform/[webform_id].tsx
@@ -2,16 +2,21 @@ import * as React from 'react';
 import {
   components,
   resolveWebformContent,
-  Webform
+  Webform,
+  defaultUiStrings
 } from 'nextjs-drupal-webform';
-import { drupal } from '../../lib/drupal';
-import { GetStaticPathsContext } from 'next/types';
-import { GetStaticPathsResult } from 'next';
-import { Layout } from '../../components/layout';
-import { getMenus } from '../../lib/get-menus';
-import { PageHeader } from '../../components/page-header';
+import {drupal} from '../../lib/drupal';
+import {GetStaticPathsContext} from 'next/types';
+import {GetStaticPathsResult} from 'next';
+import {Layout} from '../../components/layout';
+import {getMenus} from '../../lib/get-menus';
+import {PageHeader} from '../../components/page-header';
 import withCustomStyles from "../../components/webform/withCustomStyles";
 import classNames from "classnames";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import {useTranslation, Trans} from 'next-i18next'
+import {useRouter} from "next/router";
+import Link from 'next/link'
 
 const labelProps = {
   className: classNames(['block', 'text-gray-700', 'text-sm', 'font-bold', 'mb-2']),
@@ -22,18 +27,39 @@ const fieldProps = {
 const wrapperProps = {
   className: classNames(['space-y-3']),
 };
-export default function WebformSlug({ menus, webform, id }) {
+export default function WebformSlug({menus, webform, id}) {
+  const router = useRouter();
+  const {t} = useTranslation('common');
+  const changeTo = router.locale === 'en' ? 'de' : 'en'
+
   return (
     <Layout title={webform.title} menus={menus}>
-      <PageHeader heading={webform.title} />
+      {/*<PageHeader heading={webform.title}/>*/}
+      <PageHeader heading={t('title')}/>
+      <Link
+        href='/webform/default_test_form'
+        locale={changeTo}
+      >
+        <button>
+          {t('change-locale', {changeTo})}
+        </button>
+      </Link>
       <div className="container px-6 pb-10 mx-auto">
         <Webform id={id} data={webform} customComponents={{
           textfield: withCustomStyles(components.textfield, fieldProps, labelProps, wrapperProps),
           select: withCustomStyles(components.select, fieldProps, labelProps, wrapperProps),
           webform_autocomplete: withCustomStyles(components.webform_autocomplete, fieldProps, labelProps, wrapperProps),
           textarea: withCustomStyles(components.textarea, fieldProps, labelProps, wrapperProps),
-          webform_actions: withCustomStyles(components.webform_actions, { className: classNames('bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded')})
-        }} />
+          webform_actions: withCustomStyles(components.webform_actions, {className: classNames('bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded')})
+        }}
+                 uiStrings={{
+                   ...defaultUiStrings,
+                   foo: (() => {
+                     return 'Hello from France {0} {1}' + Math.random();
+                   })(),
+                 }}
+                 translate={t}
+        />
       </div>
     </Layout>
   );
@@ -44,7 +70,7 @@ export async function getStaticPaths(
 ): Promise<GetStaticPathsResult> {
   const entities = await drupal.getResourceCollectionFromContext('webform--webform', context);
   const paths = entities.map((entity) => {
-    return { params: { webform_id: entity.drupal_internal__id }}
+    return {params: {webform_id: entity.drupal_internal__id}}
   });
   return {
     paths: [...paths],
@@ -53,6 +79,7 @@ export async function getStaticPaths(
 }
 
 export async function getStaticProps(context) {
+  console.log(context);
   const webform = await resolveWebformContent(
     context.params.webform_id,
     drupal,
@@ -63,6 +90,7 @@ export async function getStaticProps(context) {
       webform,
       id: context.params.webform_id,
       menus: await getMenus(),
+      ...(await serverSideTranslations('locale', ['common', 'footer'], null, ['en', 'de'])),
     },
     revalidate: 1,
   };
